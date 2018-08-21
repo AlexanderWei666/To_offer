acb3d09543488af94f7fe86e3ba21cced4ef65d6
AlexanderWei <alexanderwei0013@gmail.com>
2018-08-21 14:52:03 +0800

    分别递归和循环实现

---
+++ src/main/java/com/alexander/Test05.java
@@ -0,0 +1,44 @@
+package com.alexander;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Test05 {
+
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> list = new ArrayList();
+//        //循环实现
+//        Stack<Integer> stack = new Stack<>();
+//        while (listNode != null)
+//        {
+//            stack.push(listNode.val);
+//            listNode = listNode.next;
+//        }
+//        while (!stack.isEmpty())
+//            list.add(stack.pop());
+
+        addNumberToArray(list, listNode);
+        return list;
+
+    }
+
+
+    public static void addNumberToArray(ArrayList<Integer> list, ListNode listNode) {
+        //递归实现
+        if (listNode == null)
+            return;
+        addNumberToArray(list, listNode.next);
+        list.add(listNode.val);
+    }
+
+}
