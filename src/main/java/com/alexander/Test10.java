dc3c7cc80bf6fe8eb33c9c6672cc588fa1cb182d
AlexanderWei <alexanderwei0013@gmail.com>
2018-08-23 14:15:36 +0800

    青蛙跳台阶，设f(n)为青蛙跳n级台阶的种数，则要么从n-1跳上来，要么从n-2跳上来，则得到递推公式f(n) = f(n -1) + f(n -2)

---
+++ src/main/java/com/alexander/Test10.java
@@ -0,0 +1,29 @@
+package com.alexander;
+
+/**
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
+ */
+public class Test10 {
+    public int JumpFloor(int target) {
+        //递归方法，效率低
+        /*if(target == 0)
+            return 1;
+        if(target == 1)
+            return 1;
+        return JumpFloor(target - 1) + JumpFloor(target - 2);*/
+
+        //循环方法
+        if(target <= 0) return 0;
+        if(target == 1) return 1;
+        if(target == 2) return 2;
+        int one = 1;
+        int two = 2;
+        int result = 0;
+        for(int i = 2; i < target; i++){
+            result = one+ two;
+            one = two;
+            two = result;
+        }
+        return result;
+    }
+}
