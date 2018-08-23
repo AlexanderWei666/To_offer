58ccbc15686fb128573bf1b2c4196dabe07ba99d
AlexanderWei <alexanderwei0013@gmail.com>
2018-08-23 17:55:34 +0800

    添加测试用例

--- src/main/java/com/alexander/Test13.java
+++ src/main/java/com/alexander/Test13.java
@@ -4,11 +4,11 @@ package com.alexander;
  * 输入一个链表，输出该链表中倒数第k个结点。
  */
 public class Test13 {
-    public ListNode FindKthToTail(ListNode head, int k) {
+    public static ListNode FindKthToTail(ListNode head, int k) {
         //边界条件处理
         if (head == null)
             return null;
-        if(k == 0)
+        if (k == 0)
             return null;
 
         ListNode l1 = head;
@@ -20,11 +20,27 @@ public class Test13 {
             else return null;//处理链表长度小于k的情况
         }
 
-        while(l2.next != null){
+        while (l2.next != null) {
             l1 = l1.next;
             l2 = l2.next;
         }
 
         return l1;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(0);
+        ListNode listNode = listNode1;
+        for (int i = 1; i < 10; i++) {
+            listNode.next = new ListNode(i);
+            listNode = listNode.next;
+        }
+        ListNode listNode2 = null;
+
+        listNode = FindKthToTail(listNode1, 4);
+        System.out.println(listNode.val);
+        System.out.println(FindKthToTail(listNode1, 20));
+        System.out.println(FindKthToTail(listNode1, 0));
+        System.out.println(FindKthToTail(listNode2, 1));
     }
 }
