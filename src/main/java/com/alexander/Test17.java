77820414a6654a44ec998b362b57692012f2923f
AlexanderWei <alexanderwei0013@gmail.com>
2018-08-24 11:20:13 +0800

    输入两个单调递增的链表，输出两个链表合成后的链表，当然我们需要合成后的链表满足单调不减规则。

--- src/main/java/com/alexander/Test17.java
+++ src/main/java/com/alexander/Test17.java
@@ -8,7 +8,23 @@ import java.util.Stack;
 
 public class Test17 {
     public static ListNode merge(ListNode list1, ListNode list2) {
-        //边界条件处理
+        //递归方法
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        }
+        ListNode pMergeHead = null;
+        if (list1.val < list2.val) {
+            pMergeHead = list1;
+            pMergeHead.next = merge(list1.next, list2);
+        } else {
+            pMergeHead = list2;
+            pMergeHead.next = merge(list1, list2.next);
+        }
+        return pMergeHead;
+
+        /*//边界条件处理
         if (list1 == null && list2 == null) {
             return null;
         } else if (list1 != null && list2 == null) {
@@ -49,7 +65,7 @@ public class Test17 {
         }
         if (list1IsHead)
             return list1;
-        else return list2;
+        else return list2;*/
     }
 
     public static void main(String[] args) {
